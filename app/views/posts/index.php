483ca94ca84efdb965f8d0cc63499a9df5dd2753
Youness Benbraitit <ybenbrai@e1r8p2.1337.ma>
2020-02-14 02:30:25 +0100

    --- Auto-Git Commit ---

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -25,7 +25,6 @@
             </form>
             </div>
             <div class="reaction">
-                
                 <div class="likes">
                     <form action="<?php echo URLROOT; ?>/posts/like/<?php echo $post->postId; ?>" method="POST">
                         <button  class="likes" type="submit"><a ><?php echo $post->like_count; ?>  </a></button>
