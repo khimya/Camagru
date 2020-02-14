727fd025d3da632f9fe7b0d62b30ed94bbdeaa49
Youness Benbraitit <ybenbrai@e1r8p2.1337.ma>
2020-02-14 02:20:09 +0100

    --- Auto-Git Commit ---

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -19,17 +19,25 @@
                 <a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->postId; ?>"><img href alt="Anne Hathaway picture" src="<?php echo $post->image; ?>"></a>
             </div>
             <div class="blabla">
-            <form action="" method="post">
-                <textarea name="blabla" id="commntarea" cols="50" rows="2"></textarea>
-                <button class="publish"  type="submit">Publish</button>
-            </form>
+                <form action="" method="post">
+                    <textarea name="blabla" id="commntarea" cols="50" rows="2"></textarea>
+                    <button class="publish" type="submit">Publish</button>
+                </form>
             </div>
+            <div class="reaction">
+                        <div class="user__comment__reaction">
+                            <i class="icon ion-md-thumbs-up"></i> <small>34</small>
+                            <i class="icon ion-md-thumbs-down"></i> <small>04</small>
+                        </div>
+                    </div>
+                </div>
                     <form action="<?php echo URLROOT; ?>/posts/like/<?php echo $post->postId; ?>" method="POST">
-                        <button  class="likes" type="submit"><a ><?php echo $post->like_count; ?>  </a></button>
+                        <button class="likes" type="submit"><a><?php echo $post->like_count; ?> </a></button>
                         <!-- <input type="submit" value="<?php echo $post->like_count; ?>"/> -->
                     </form>
 
                     <a href=""><?php echo $post->like_count; ?></a>
+            </div>
             <div class="posts_info">
                 <p>
                     Written by <?php echo $post->display_name; ?> on <?php echo $post->created_at; ?>
