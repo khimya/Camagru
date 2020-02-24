af8218d0947e157432284e4d2de1bc55590a767e
Youness Benbraitit <ybenbrai@e1r8p2.1337.ma>
2020-02-24 23:02:15 +0100

    --- Auto-Git Commit ---

--- app/views/inc/footer.php
+++ app/views/inc/footer.php
@@ -1,6 +1,16 @@
 </div>
 <script src="<?php echo URLROOT; ?>/js/navbar.js"></script>
 <script src="<?php echo URLROOT; ?>/js/upload.js"></script>
+<div class="pagination">
+  <a href="#">&laquo;</a>
+  <a href="#">1</a>
+  <a class="active" href="#">2</a>
+  <a href="#">3</a>
+  <a href="#">4</a>
+  <a href="#">5</a>
+  <a href="#">6</a>
+  <a href="#">&raquo;</a>
+</div>
  
 </body>
 
